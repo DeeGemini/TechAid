5fb71b92fd2ed37573a09e9ee6778e0d5c4e742a
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2025-01-26 19:52:43 +0300

    feat: google auth

---
+++ backend/app/api/endpoints/google_auth.py
@@ -0,0 +1,50 @@
+from fastapi import APIRouter, Request, HTTPException, status
+from fastapi.responses import RedirectResponse
+from starlette.config import Config
+from authlib.integrations.starlette_client import OAuth, OAuthError
+
+from app.core.config import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET
+
+
+if not GOOGLE_CLIENT_ID or not GOOGLE_CLIENT_SECRET:
+    raise ValueError("Missing Google credentials!")
+
+config_data = {'GOOGLE_CLIENT_ID': GOOGLE_CLIENT_ID, 'GOOGLE_CLIENT_SECRET': GOOGLE_CLIENT_SECRET}
+config = Config(environ=config_data)
+oauth = OAuth(config=config)
+
+oauth.register(
+    name='google',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={'scope': 'openid email profile'}
+)
+
+google = APIRouter()
+
+@google.route('/login')
+async def login(request: Request):
+    """
+    Login with Google.
+    """
+    redirect_uri = request.url_for('auth')
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+@google.route('/auth')
+async def auth(request: Request):
+    """
+    Authorization from the user.
+    """
+    try:
+        token = await oauth.google.authorize_access_token(request)
+    except OAuthError as error:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f'Could not validate credentials: {error}'
+        ) from error
+    print(token.get('userinfo'))
+
+    return RedirectResponse(url='/test')
+
+@google.get('/test')
+def test():
+    return {'message': 'successful'}
